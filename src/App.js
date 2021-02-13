b93b30e08fba42043345e737d71439da28259171
Fabian <fabianffc@gmail.com>
2021-02-13 13:41:49 -0600

    set up with try, catch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+  const [users, setUser] = useState([])
+
+  useEffect(()=>{
+    console.log("useEffect");
+/*
+    write a function that fetches the API data
+      try-> make the api call
+        setState of the users []
+      catch -> console.log error
+
+    invoke the funciton
+    pass in the dependency array BECAUSE, 
+    it is needed only the first time the component mounts (componentDidMount)
+    */
+    const fetchData = async () => {
+      try{
+        const response = await fetch("https://randomuser.me/api/?results=10")
+        const data = await response.json()
+        setUser([...data.results])
+
+      }catch(e){
+        console.log(e);
+      }
+    }
+    fetchData()
+  },[])
+
+console.log("users1", users);
+useEffect(()=>console.log("users2",users))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     hi
     </div>
   );
 }
